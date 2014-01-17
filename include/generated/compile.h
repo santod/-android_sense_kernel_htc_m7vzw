8c21680779618b7d59259ae4bec07e3aa674938c
santod <santod040@gmail.com>
2014-01-17 12:15:17 -0800

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Jan 17 04:01:00 PST 2014"
+#define LINUX_COMPILE_BY "santod"
+#define LINUX_COMPILE_HOST "DevBoX"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
